6fbc1276a4afaa274954f95f4697ef4acea7b627
Roy Brody <Roy.Brody@cellebrite.com>
2021-10-19 11:27:00 +0300

    start console project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1(1, "class");
+
+            Record1 record1 = new Record1(2, "record");
+
+            Console.WriteLine($"Class => {class1.Id}-{class1.Name}");
+            Console.WriteLine($"Record => {record1.Id}-{record1.Name}");
+
+            //record1.Name = "et";
+
+        }
+    }
+}
